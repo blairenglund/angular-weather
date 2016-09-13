a16dbfc9677e2c4f50bbc02d668cab4df419e78d
Blair Englund <blair.englund@gmail.com>
2016-09-12 21:46:04 -0500

    pushing

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,5 +10,15 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [function() {
-
+	$http({
+	  method: 'GET',
+	  url: 'http://api.openweathermap.org/data/2.5/weather?id=5074472&APPID=0aa45db4ad14dde4654b8554739f9f2e'
+	}).then(function successCallback(response) {
+	    // this callback will be called asynchronously
+	    // when the response is available
+	  }, function errorCallback(response) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
 }]);
+
